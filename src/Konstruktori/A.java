d73177fd91bc353d481e650ea5cc8db14b820e0b
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2023-12-01 10:13:24 +0100

    Add changes to java project primer

--- src/Konstruktori/A.java
+++ src/Konstruktori/A.java
@@ -5,20 +5,17 @@ public class A {
     public A(int a, int b) {
         this.a = a;
         this.b = b;
-        c += a + b;
+        c+=a+b;
     }
-
     void printC() {
         System.out.println(c);
     }
-
     static class M21 {
         public static void m(int a, int b) {
             A m = new A(a, b);
             m.printC();
         }
     }
-
     public static void main(String[] args) {
         A instance = new A(2, 5);
         instance.printC();
